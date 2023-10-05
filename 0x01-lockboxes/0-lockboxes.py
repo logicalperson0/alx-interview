daeed6671bbe69efb9479bc6e701b3c7869f034a
Samuel <a2jsamuel@gmail.com>
2023-10-06 01:37:52 +0300

    change so all col is at 79

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,9 @@ Lockboxes Technical interview Qn
 
 def canUnlockAll(boxes):
     """ a method that determines if all the boxes can be opened for a:
-    n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes"""
+    n number of locked boxes in front of you. Each box is numbered
+    sequentially from 0 to n - 1 and each box may contain keys to the other
+    boxes"""
     if (len(boxes) == 0):
         return False
 
