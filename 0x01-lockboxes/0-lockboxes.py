9e77df96a755a912ee9a5ecb5fc7617bc0b9d21b
Samuel <a2jsamuel@gmail.com>
2023-10-06 01:22:08 +0300

    fix and add

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,8 +14,8 @@ def canUnlockAll(boxes):
 
     for x in keys:
         for y in boxes[x]:
-            if (y not in keys and len(boxes)):
+            if (y not in keys and len(boxes) > y):
                 keys.append(y)
-    if len(keys) == len(boxes):
+    if (len(keys) == len(boxes)):
         return True
     return False
