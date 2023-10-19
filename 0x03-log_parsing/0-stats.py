0d451f6c5abe472f1d484c510b7bac153288c3c8
Samuel <a2jsamuel@gmail.com>
2023-10-19 21:44:29 +0300

    fix and see

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,61 +6,77 @@ import sys
 from operator import itemgetter
 
 
-def log_parser(log):
+def parser(logging):
     """
-    Parses log into different fields
+    Parses log into fields
     """
-    log_fields = log.split()
-    file_size = int(log_fields[-1])
-    status_code = log_fields[-2]
-    return status_code, file_size
+    fields = logging.split()
+    size = int(fields[-1])
+    code = fields[-2]
+    return code, size
 
 
-def validate_status_code(status_code):
+def validate(logging):
     """
-    Check if status code entry is valid
+    Validates logging formatter
     """
-    valid_status_codes = ["200", "301", "400", "401",
-                          "403", "404", "405", "500"]
-    return True if status_code in valid_status_codes else False
+    if len(logging.split()) < 7:
+        return False
+    
+    else: 
+        return True
 
 
-def print_log(file_size, status_codes) -> None:
+def status_code(code):
+    """
+    Check if code entry is valid
+    """
+    valid_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+
+    if status_code in valid_codes:
+        return True
+    
+    else:
+        return False
+
+
+def printlogging(size, codes) -> None:
     """
     Prints out log files
     """
-    sorted_status_codes = sorted(status_codes.items(), key=itemgetter(0))
-    print('File size: {}'.format(file_size))
-    for code_count in sorted_status_codes:
-        key = code_count[0]
-        value = code_count[1]
-        print("{}: {}".format(key, value))
+    sorted_codes = sorted(codes.items(), key=itemgetter(0))
+    print('File size: {}'.format(size))
+    for code_count in sorted_codes:
+        keys = code_count[0]
+        values = code_count[1]
+        print("{}: {}".format(keys, values))
 
 
-def log_stats():
+def logging_stats():
     """
     Reads logs from std in and prints out statistic
     on status code and file size
     """
-    status_codes_count = {}
+    status_count = {}
     total_size = 0
     log_count = 0
     try:
         for log in sys.stdin:
             log_count += 1
-            
-            status_code, file_size = log_parser(log)
+            if not validate(log):
+                continue
+            status_code, file_size = parser(log)
             total_size += file_size
-            if validate_status_code(status_code):
+            if status_code(status_code):
                 entry = {status_code:
-                         status_codes_count.get(status_code, 0) + 1}
-                status_codes_count.update(entry)
+                         status_count.get(status_code, 0) + 1}
+                status_count.update(entry)
             if not log_count % 10:
-                print_log(total_size, status_codes_count)
+                printlogging(total_size, status_count)
     except KeyboardInterrupt:
-        print_log(total_size, status_codes_count)
-    print_log(total_size, status_codes_count)
+        printlogging(total_size, status_count)
+    printlogging(total_size, status_count)
 
 
 if __name__ == '__main__':
-    log_stats()
+    logging_stats()
