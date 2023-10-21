0d7335380ed75de93199b852f46577fc6f6b0aff
Samuel <a2jsamuel@gmail.com>
2023-10-21 14:48:40 +0300

    fix and add

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,77 +6,45 @@ import sys
 from operator import itemgetter
 
 
-def parser(logging):
-    """
-    Parses log into fields
-    """
-    fields = logging.split()
-    size = int(fields[-1])
-    code = fields[-2]
-    return code, size
-
-
-def validate(logging):
-    """
-    Validates logging formatter
-    """
-    if len(logging.split()) < 7:
-        return False
-
-    else:
-        return True
-
-
-def status_code(code):
-    """
-    Check if code entry is valid
-    """
-    valid_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-
-    if code in valid_codes:
-        return True
-
-    else:
-        return False
-
-
-def printlogging(size, codes) -> None:
-    """
-    Prints out log files
-    """
-    sorted_codes = sorted(codes.items(), key=itemgetter(0))
-    print('File size: {}'.format(size))
-    for code_count in sorted_codes:
-        keys = code_count[0]
-        values = code_count[1]
-        print("{}: {}".format(keys, values))
-
-
-def logging_stats():
-    """
-    Reads logs from std in and prints out statistic
-    on status code and file size
-    """
-    status_count = {}
+def log_stats():
+    """Reads from stdin from piping"""
+    count = 0
     total_size = 0
-    log_count = 0
+    status_code = ['200', '301', '400', '401', '403', '404', '405', '500']
+    valid_codes_count = {}
+
     try:
-        for log in sys.stdin:
-            log_count += 1
-            if not validate(log):
-                continue
-            status_code, file_size = parser(log)
+        for line in sys.stdin:
+
+            fields = line.split()
+
+            file_size = int(fields[-1])
+            valid_codes = fields[-2]
+            if valid_codes in status_code:
+                if valid_codes_count.get(valid_codes, -1) == -1:
+                    valid_codes_count[valid_codes] = 1
+                else:
+                    valid_codes_count[valid_codes] += 1
+
+                
             total_size += file_size
-            if status_code(status_code):
-                entry = {status_code:
-                         status_count.get(status_code, 0) + 1}
-                status_count.update(entry)
-            if not log_count % 10:
-                printlogging(total_size, status_count)
+
+            if count == 10:
+                print ("File size: {}".format(total_size))
+                for status in sorted(valid_codes_count):
+                    print ('{}: {}'.format(status, valid_codes_count[status]))
+                count = 1
+            else:
+                count += 1
+        print ("File size: {}".format(total_size))
+        for status in sorted(valid_codes_count):
+            print ('{}: {}'.format(status, valid_codes_count[status]))
+    
     except KeyboardInterrupt:
-        printlogging(total_size, status_count)
-    printlogging(total_size, status_count)
+        print ("File size: {}".format(total_size))
+        for status in sorted(valid_codes_count):
+            print ('{}: {}'.format(status, valid_codes_count[status]))
+            
 
-
-if __name__ == '__main__':
-    logging_stats()
+if __name__ == "__main__":
+    log_stats()
