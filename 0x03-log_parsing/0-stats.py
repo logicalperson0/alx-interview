25fff64a538b81de84c88bee4711fca237efca4d
Samuel <a2jsamuel@gmail.com>
2023-10-21 15:45:52 +0300

    tetsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,50 @@
 #!/usr/bin/python3
 """
-module for script that reads stdin line by line and computes metrics
+log parsing
 """
+
 import sys
-from operator import itemgetter
+import re
 
 
-def log_stats():
-    """Reads from stdin from piping"""
-    count = 0
-    total_size = 0
-    status_code = ['200', '301', '400', '401', '403', '404', '405', '500']
-    valid_codes_count = {}
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print("File size: {}".format(total_size))
-                for status in sorted(valid_codes_count):
-                    print('{}: {}'.format(status, valid_codes_count[status]))
-                count = 1
-            else:
-                count += 1
-
-            fields = line.split()
-
-            file_size = int(fields[-1])
-            valid_codes = fields[-2]
-            try:
-                if valid_codes in status_code:
-                    if valid_codes_count.get(valid_codes, -1) == -1:
-                        valid_codes_count[valid_codes] = 1
-                    else:
-                        valid_codes_count[valid_codes] += 1
-            except IndexError:
-                pass
-
-            try:
-                total_size += file_size
-            except(IndexError, ValueError):
-                pass
-
-        print("File size: {}".format(total_size))
-        for status in sorted(valid_codes_count):
-            print('{}: {}'.format(status, valid_codes_count[status]))
-
-    except KeyboardInterrupt:
-        print("File size: {}".format(total_size))
-        for status in sorted(valid_codes_count):
-            print('{}: {}'.format(status, valid_codes_count[status]))
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
 if __name__ == "__main__":
-    log_stats()
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
