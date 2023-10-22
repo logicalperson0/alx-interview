80837efe7ab11a35ce34ab525025954dfa3cc9f6
Samuel <a2jsamuel@gmail.com>
2023-10-23 01:40:50 +0300

    mod and see

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,16 +7,9 @@ A module with a fun: minOperations
 def minOperations(n):
     """method that calculates the fewest number of operations needed to
     result in exactly n H characters in the file"""
-    chars_pro = 2
-    opers = 0
-
-    if n < 1:
+    if n == 1:
         return 0
-
-    while n > 1:
-        while (n % chars_pro == 0):
-            n /= chars_pro
-            opers += chars_pro
-        chars_pro += 1
-
-    return opers
+    elif (n % 2 == 0):
+        return minOperations(n // 2) + 1
+    else:
+        return minOperations(n // 2 + 1) + 2
