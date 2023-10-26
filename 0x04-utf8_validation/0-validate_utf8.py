c6c07cf5460e8aa5ad6a7b3927610abaad86bbb9
Samuel <a2jsamuel@gmail.com>
2023-10-26 23:50:52 +0300

    fix and add for handling valueerror

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,10 +7,11 @@ a module with a single function called validUTF8
 def validUTF8(data):
     """method that determines if a given data set represents
     a valid UTF-8 encoding"""
-    to_bytes = bytes(data)
+    # to_bytes = bytes(data)
     # print(to_bytes)
     try:
-        to_bytes.decode('utf-8')
-    except UnicodeDecodeError:
+        bytes(data).decode('utf-8')
+        return True
+    except (UnicodeDecodeError, ValueError):
         return False
-    return True
+    # return True
