d291b0ebb2b5c184e11678fd0088cccd09efca4b
Samuel <a2jsamuel@gmail.com>
2024-02-02 02:23:32 +0300

    chk this test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,7 +8,8 @@ def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
     if (total <= 0):
         return 0
-    coins.sort()
+    #coins.sort()
+    """
     i = -1
     num_coins = 0
     nums = 0
@@ -34,3 +35,16 @@ def makeChange(coins, total):
                         return -1
                     elif nums == total:
                         return num_coins
+      """
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
