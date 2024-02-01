f3660d3a8ef7c00fa76904c690f74106490213e7
Samuel <a2jsamuel@gmail.com>
2024-02-02 01:56:38 +0300

    this is for the tech qn for task 0 on making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Making changes
+"""
+
+
+def makeChange(coins, total):
+  """Return: fewest number of coins needed to meet total"""
+  if (total <= 0):
+    return 0
+  coins.sort()
+  i = -1
+  num_coins = 0
+  nums = 0
+
+  while 1:
+      nums += coins[i]
+      num_coins += 1
+      if nums > total:
+          nums -= coins[i]
+          num_coins -= 1
+          i -= 1
+      elif nums == total:
+          return num_coins
+      elif nums < total:
+          if ((nums + coins[i]) > total):
+              i -= 1
+          if coins[i] == coins[0]:
+
+              while 1:
+                  nums += coins[0]
+                  num_coins += 1
+                  if nums > total:
+                      return -1
+                  elif nums == total:
+                      return num_coins
